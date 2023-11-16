b39842b1279789606e1c5265addf0843fb151e78
Tripl3Design <boudewijn@tripledesign.nl>
2023-11-16 10:28:13 +0100

    filestructure

--- dist/projects/pastoe-amsterdammer/search.js
+++ dist/projects/pastoe-amsterdammer/search.js
@@ -48,7 +48,6 @@ function addDecor(modelType, modelWidth, modelHeight, modelDepth, TvHeight, TvDi
 }
 
 function showSearchImages(modelFromSearch) {
-    // get width and corresponding type
     const widths = [
         { "width": 37, "type": "cabinet" },
         { "width": 55, "type": "cabinet" },
@@ -59,7 +58,7 @@ function showSearchImages(modelFromSearch) {
         { "width": 194, "type": "sideboard" },
         { "width": 194, "type": "sideboardOnFrame" }
     ];
-    const filteredWidth = widths.filter(item => item.width <= modelFromSearch.width);
+    const filteredWidth = widths.filter(item => item.width >= modelFromSearch.width.min && item.width <= modelFromSearch.width.max);
 
     const heights = [
         { "height": 75, "type": "sideboard" },
@@ -69,16 +68,19 @@ function showSearchImages(modelFromSearch) {
         { "height": 205, "type": "cabinet" },
         { "height": 221, "type": "cabinet" }
     ];
-    const filteredHeight = heights.filter(item => item.height <= modelFromSearch.height);
+    const filteredHeight = heights.filter(item => item.height >= modelFromSearch.height.min && item.height <= modelFromSearch.height.max);
 
     let randomType, randomWidthType, randomHeightType, randomWidth, randomHeight;
 
-    while (true) {
+    const maxAttempts = 20;
+    let attempts = 0;
+
+    while (attempts < maxAttempts) {
+        document.getElementById('searchTitle').textContent = '';
         if (filteredWidth.length === 0 || filteredHeight.length === 0) {
-            // Handle the case where no matching items were found
             console.log('No matching items found.');
             document.getElementById('searchTitle').textContent = 'No matching items found.';
-            break; // Exit the loop
+            break;
         }
 
         const randomWidthIndex = Math.floor(Math.random() * filteredWidth.length);
@@ -92,17 +94,20 @@ function showSearchImages(modelFromSearch) {
         randomHeight = randomHeightItem.height;
 
         if (randomWidthType === randomHeightType) {
-            // Types match, you can perform additional actions here
             randomType = randomWidthType;
             console.log('Types match:', randomType);
-            break; // Exit the loop
+            break;
+        }
+        else if (attempts === maxAttempts) {
+            console.log('No matching items found after ' + maxAttempts + ' attempts.');
+            document.getElementById('searchTitle').textContent = 'No matching items found.';
+            break;
         } else {
-            // Types don't match, repeat the loop
+            attempts++;
             console.log('Types do not match. Retrying...');
         }
     }
 
-    // get random color in selected colorGroup
     const colorGroup = ALLCOLORS.outsideColors.filter(color => color.colorGroup === modelFromSearch.color);
     if (colorGroup.length === 0) {
         console.log("There are no colors in this colorGroup");
@@ -111,7 +116,6 @@ function showSearchImages(modelFromSearch) {
     const randomColorGroup = colorGroup[randomColorGroupIndex].colorHex;
     let randomOutsideColor;
     if (randomType === 'cabinet' && colorGroup[randomColorGroupIndex].colorPath) {
-        // If modelFromSearch.type is 'cabinet' and colorPath is defined
         randomOutsideColor = {
             color: randomColorGroup,
             path: colorGroup[randomColorGroupIndex].colorPath,
@@ -128,7 +132,7 @@ function showSearchImages(modelFromSearch) {
             };
         }
     }
-    
+
     console.log(randomType);
     console.log(randomOutsideColor);
 
@@ -160,8 +164,8 @@ function showSearchImages(modelFromSearch) {
     const btn = document.getElementById('goToConfigurator');
 
     btn.addEventListener('click', (e) => {
-        //window.location.href = `${document.referrer}?brand=${brand}&product=${product}&data=${encodeURIComponent(JSON.stringify(model))}`;
-        window.location.href = `https://furnitise.nl?noDecor&noFeaturedModels&noType&brand=${brand}&product=${product}&data=${encodeURIComponent(JSON.stringify(model))}`;
+        window.location.href = `${document.referrer}?brand=${brand}&product=${product}&data=${encodeURIComponent(JSON.stringify(model))}`;
+        //window.location.href = `https://furnitise.nl?noDecor&noFeaturedModels&noType&brand=${brand}&product=${product}&data=${encodeURIComponent(JSON.stringify(model))}`;
     });
 
     document.getElementById('productBrand').src = `https://${brand}-${product}.web.app/img/logo_${brand}.svg`;
@@ -170,28 +174,4 @@ function showSearchImages(modelFromSearch) {
     pricing(model);
 
     generateRenderTexture('search', model);
-}
-
-async function initUnity() {
-    console.log('INIT UNITY')
-    var canvas = document.getElementById("modelviewer");
-    var buildUrl = `https://${brand}-${product}.web.app/projects/${brand}-${product}`;
-    var config = {
-        dataUrl: `${buildUrl}/Build/${brand}-${product}.data`,
-        frameworkUrl: `${buildUrl}/Build/${brand}-${product}.framework.js`,
-        codeUrl: `${buildUrl}/Build/${brand}-${product}.wasm`,
-        //streamingAssetsUrl: "StreamingAssets",
-        companyName: 'TripleDesign',
-        productName: product.charAt(0).toUpperCase() + product.slice(1),
-        productVersion: '0.1',
-    };
-
-    const unityPromise = createUnityInstance(canvas, config, (progress) => {
-        progressBar.style.width = 100 * progress + '%';
-    });
-    const colorsPromise = fetch(`${buildUrl}/colors.json`).then(response => response.json());
-    const componentsPromise = fetch(`${buildUrl}/components.json`).then(response => response.json());
-    UNITY_INSTANCE = await unityPromise;
-    ALLCOLORS = await colorsPromise;
-    ALLCOMPONENTS = await componentsPromise;
 }
---
+++ public/js/initUnity.js
@@ -0,0 +1,41 @@
+async function initUnity() {
+    const configurators = [{
+        "brand": 'pastoe',
+        "product": 'amsterdammer',
+        "title": 'a\'dammer'
+    },
+    {
+        "brand": 'pastoe',
+        "product": 'frame',
+        "title": 'frame'
+    }
+    ];
+
+    const randomConfigurator = configurators[Math.floor(Math.random() * configurators.length)];
+    console.log("RANDOM " + randomConfigurator.product);
+
+    const brand = randomConfigurator.brand;
+    const product = randomConfigurator.product;
+    const title = randomConfigurator.title;
+
+    var canvas = document.getElementById("modelviewer");
+    var buildUrl = `https://${brand}-${product}.web.app/projects/${brand}-${product}`;
+    var config = {
+        dataUrl: `${buildUrl}/Build/${brand}-${product}.data`,
+        frameworkUrl: `${buildUrl}/Build/${brand}-${product}.framework.js`,
+        codeUrl: `${buildUrl}/Build/${brand}-${product}.wasm`,
+        //streamingAssetsUrl: "StreamingAssets",
+        companyName: 'TripleDesign',
+        productName: product.charAt(0).toUpperCase() + product.slice(1),
+        productVersion: '0.1',
+    };
+
+    const unityPromise = createUnityInstance(canvas, config, (progress) => {
+        progressBar.style.width = 100 * progress + '%';
+    });
+    const colorsPromise = fetch(`${buildUrl}/colors.json`).then(response => response.json());
+    const componentsPromise = fetch(`${buildUrl}/components.json`).then(response => response.json());
+    UNITY_INSTANCE = await unityPromise;
+    ALLCOLORS = await colorsPromise;
+    ALLCOMPONENTS = await componentsPromise;
+}
